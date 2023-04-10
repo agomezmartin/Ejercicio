dbe43b9c031b6a12deaf72c719b69827043a68cc
Alberto G <agomezmartinprorgmail.com>
2023-04-10 20:15:57 +0200

    Semáforo terminado

--- src/app/semaforo/semaforo.component.ts
+++ src/app/semaforo/semaforo.component.ts
@@ -10,9 +10,42 @@ export class SemaforoComponent implements OnInit {
   btn = "Siguiente";
   circulo = "circulo";
   negro = "negro";
-  colores = ["verde", "ambar", "rojo"];
+/*  colores = ["verde", "ambar", "rojo"];
+*/
+  color1 = "negro";
+  color2 = "negro";
+  color3 = "negro";
+
+  control: number = 0;
 
   cambiarColor(){
+
+    if(this.control===0){
+      this.color1 = "verde";
+      this.color2 = "negro";
+      this.color3 = "negro";
+      this.control++;
+      
+    } else if (this.control === 1){
+      this.color1 = "negro";
+      this.color2 = "ambar";
+      this.color3 = "negro";
+      this.control++;
+
+    } else if (this.control === 2){
+      this.color1 = "negro";
+      this.color2 = "negro";
+      this.color3 = "rojo";
+      this.control++;
+    } else {
+      this.control = 0;
+      this.color1 = "negro";
+      this.color2 = "negro";
+      this.color3 = "negro";      
+    }
+
+
+
 
   }
 
